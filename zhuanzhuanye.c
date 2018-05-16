18022ad8dc6094997975eee45f09b279143ca572
王舟 <wangzhou5130@gmail.com>
2018-05-17 07:55:18 +0800

    转专业算法设计题

---
+++ zhuanzhuanye.c
@@ -0,0 +1,45 @@
+// 计算一棵二叉树中关键字等于x的结点个数：
+
+// 定义二叉树的结构体和二叉树结点类型的结构体
+typedef struct btnode{
+    struct btnode *left;
+    struct btnode *right;
+    int data;
+}BTnode;
+
+typedef struct bitree{
+    struct btnode *root;
+}Btree;
+
+// count在main主函数里初始化为0
+void Count(Btree *t,int *count,int x){
+    if(t == NULL){
+        return;
+    }
+    if(t->root->data == x){
+        count += 1;  
+    }
+    Count(t->left,&count,x);  //因为把count定义为指针变量，传的是count的地址，所以要加&
+    Count(t->right,&count,x);
+}
+
+
+
+//简单选择排序算法
+int SelectSort(int a[],int n){
+    int i,j,small,x;
+    for(i = 0;i < n;i ++){
+        small = i;
+        for(j = i + 1;j < n;j ++){
+            if(a[small] > a[j]){
+                small = j;
+            }
+        }
+        if(small != i){
+            x = a[i];
+            a[i] = a[small];
+            a[small] = x;
+        }
+    }
+    return 0;
+}
