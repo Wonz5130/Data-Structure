8832e4201b6d4aa31b9f4e4e753d029e844489cd
王舟 <wangzhou5130@gmail.com>
2018-07-29 15:45:44 +0800

    Add files via upload

---
+++ Experiments/Experiment-3/shiyan3.3.cpp
@@ -0,0 +1,143 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include <windows.h>
+#define ERROR 0
+#define OK 1
+#define Overflow 2     //表示上溢
+#define Underflow 3    //表示下溢
+#define NotPresent 4   //表示元素不存在
+#define Duplicate 5    //表示有重复元素
+typedef int ElemType;
+typedef int Status;
+
+
+//邻接表的结构体定义
+typedef struct ENode{
+    int adjVex;             //任意顶点u相邻的顶点
+    ElemType w;             //边的权值
+    struct ENode *nextArc;  //指向下一个边结点
+}ENode;
+
+typedef struct{
+    int n;             //图的当前顶点数
+    int e;             //图的当前边数
+    ENode **a;         //指向一维指针数组
+}LGraph;
+
+
+//邻接表的初始化
+Status Init(LGraph *lg,int nSize){
+    int  i;
+    lg->n = nSize;
+    lg->e = 0;
+    lg->a = (ENode**)malloc(nSize*sizeof(ENode*));  //动态生成长度为n的一维指针数组
+    if(!lg->a) return ERROR;
+    else{
+        for(i = 0;i < lg->n;i ++){
+            lg->a[i] = NULL;                        //将指针数组a置空
+        }
+        return OK;
+    }
+}
+
+
+//邻接表的撤销(改成了int型，有返回值)
+int Destory(LGraph *lg){
+    int i;
+    ENode *p,*q;
+    for(i = 0;i < lg->n;i ++){
+        p = lg->a[i];                 //指针p指向顶点i的单链表的第一个边结点
+        q = p;
+        while(p){                     //释放顶点i的单链表中所有边结点
+            p = p->nextArc;
+            free(q);
+            q = p;
+        }
+    }
+    free(lg->a);                     //释放一维指针数组a的存储空间
+    return 1;                        //改为int型函数,有返回值
+}
+
+
+//邻接表的搜索边
+Status Exist(LGraph *lg,int u,int v){
+    ENode *p;
+    if(u < 0||v < 0||u > lg->n-1 ||v > lg->n-1 ||u == v) return ERROR;
+    p = lg->a[u];                   //指针p指向顶点u的单链表的第一个边结点
+    while(p!=NULL && p->adjVex != v){
+        p = p->nextArc;
+    }
+    if(!p) return ERROR;            //若未找到此边,则返回ERROR
+    else return OK;
+}
+
+
+//邻接表的插入边
+Status Insert(LGraph *lg,int u,int v,ElemType w){
+    ENode *p;
+    if(u < 0||v < 0||u > lg->n-1||v > lg->n-1 ||u == v) return ERROR;
+    if(Exist(lg,u,v)) return Duplicate;  //此边已存在,返回错误
+    p = (ENode*)malloc(sizeof(ENode));   //为新的边结点分配存储空间
+    p->adjVex = v;
+    p->w = w;
+    p -> nextArc = lg->a[u];             //将新的边结点插入单链表的最前面
+    lg->a[u] = p;
+    lg->e ++;                            //边加1
+    return OK;
+}
+
+
+//邻接表的删除边
+Status Remove(LGraph *lg,int u,int v){
+    ENode *p,*q;
+    if(u < 0||v < 0||u > lg->n-1||v > lg->n-1 ||u == v) return ERROR;
+    p = lg->a[u];
+    q = NULL;
+    while(p && p->adjVex != v){         //查找待删除边是否存在
+        q = p;
+        p = p->nextArc;
+    }
+    if(!p) return NotPresent;          //p为空,待删除边不存在
+    if(q) q->nextArc = p->nextArc;     //从单链表删除此边
+    else lg->a[u] = p->nextArc;
+    free(p);
+    lg->e --;
+    return OK;
+}
+
+
+
+int main(){
+    LGraph g;
+    int i,u,v,enode,edge;
+    ElemType w;
+    printf("Please enter the number of the ENodes:");
+    scanf("%d",&enode);
+    Init(&g,enode);
+    printf("Please enter the number of the edges:");
+    scanf("%d",&edge);
+    printf("Now init the graph.\n");
+    for(i = 0;i < edge;i ++){
+        printf("Please enter the edge:");
+        scanf("%d%d%d",&u,&v,&w);
+        Insert(&g,u,v,w);
+    }
+    //delete one edge
+    printf("Please enter the deleted edge:");
+    printf("\nPlease enter the u of the edge:");
+    scanf("%d",&u);
+    printf("Please enter the v of the edge:");
+    scanf("%d",&v);
+    printf("Now search the edge:");
+    if(Exist(&g,u,v)) printf("OK");
+    else printf("ERROR");
+    printf("\nNow delete the edge:");
+    //search the deleted edge
+    if(Remove(&g,u,v)) printf("OK");
+    else printf("ERROR");
+    //destory
+    printf("\nNow destory the graph:");
+    if(Destory(&g)) printf("OK");
+    else printf("ERROR");
+    return 0;
+}
---
+++ Experiments/Experiment-3/shiyan3.5.cpp
@@ -0,0 +1,153 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include <windows.h>
+#define ERROR 0
+#define OK 1
+#define Overflow 2     //表示上溢
+#define Underflow 3    //表示下溢
+#define NotPresent 4   //表示元素不存在
+#define Duplicate 5    //表示有重复元素
+#define INFTY 32767    //表示极大值正无穷
+typedef int ElemType;
+typedef int Status;
+
+
+//邻接矩阵的结构体定义
+typedef struct{
+    ElemType **a;     //邻接矩阵
+    int n;            //顶点数
+    int e;            //边数
+    ElemType noEdge;  //两顶点间无边时的值
+}mGraph;
+
+
+//邻接矩阵的初始化
+Status Init(mGraph *mg,int nSize,ElemType noEdgeValue){
+    int i,j;
+    mg->n = nSize;               //初始化顶点数
+    mg->e = 0;                   //初始化时没有边
+    mg->noEdge = noEdgeValue;    //初始化没有边时的取值
+    mg->a = (ElemType**)malloc(nSize*sizeof(ElemType *));  //生成长度为n的一维指针数组
+    if(!mg->a) return ERROR;
+    for(i = 0;i < mg->n;i ++){   //动态生成二维数组
+        mg->a[i] = (ElemType*)malloc(nSize*sizeof(ElemType));
+        for(j = 0;j < mg->n;j ++){
+            mg->a[i][j] = mg->noEdge;  //初始化时权重都设为-1
+        }
+        mg->a[i][i] = 0;        //自回路设置为0
+    }
+    return OK;
+}
+
+
+//邻接矩阵的撤销(改成了int型，有返回值),先释放一维数组,再释放指针数组
+int Destory(mGraph *mg){
+    int i;
+    for(i = 0;i < mg->n;i ++){
+        free(mg->a[i]);  //释放n个一维数组的存储空间
+    }
+    free(mg->a);         //释放一维数组的存储空间
+    return 1;
+}
+
+
+//邻接矩阵的边的搜索
+Status Exist(mGraph *mg,int u,int v){
+    if(u < 0||v < 0||u > mg->n-1||v > mg->n-1 ||u == v||mg->a[u][v] == mg->noEdge) return ERROR;
+    return OK;
+}
+
+
+//邻接矩阵的边的插入
+Status Insert(mGraph *mg,int u,int v,ElemType w){
+    if(u < 0||v < 0||u > mg->n-1||v > mg->n-1 ||u == v) return ERROR;
+    if(mg->a[u][v] != mg->noEdge) return Duplicate;  //若待插入边已存在,则返回出错信息
+    mg->a[u][v] = w;                                 //插入新边
+    mg->e ++;                                        //增加一条边
+    return OK;
+}
+
+
+//邻接矩阵的边的删除
+Status Remove(mGraph *mg,int u,int v){
+    if(u < 0||v < 0||u > mg->n-1||v > mg->n-1 ||u == v) return ERROR;
+    if(mg->a[u][v] == mg->noEdge) return NotPresent;  //若待删除边不存在,则返回出错信息
+    mg->a[u][v] = mg->noEdge;                         //删除边
+    mg->e --;
+    return OK;
+}
+
+
+//选出最小的d[i],i ∈ V-S
+int Choose(int d[],int n,int s[]){  //对教材进行了一点改动,形式参数设置为数组而不是指针,参考了陈惠南老师的《数据结构——C语言描述》
+    int i,minpos;
+    ElemType min;
+    min = INFTY;
+    minpos = -1;
+    for(i = 0;i < n;i ++){         //这里i初值改为0
+        if(d[i] <= min && !s[i]){  //<改为<=
+            min = d[i];
+            minpos = i;
+        }
+    }
+    return minpos;                //返回下标位置
+}
+
+
+//Dijkstra算法
+Status Dijkstra(mGraph g,int v,int d[],int path[]){  //对教材进行了一点改动,形式参数设置为数组而不是指针,参考了陈惠南老师的《数据结构——C语言描述》
+    int i,k,w,distance = 0;       //增加了一个distance记录最短距离之和
+    int *s;
+    if(v < 0||v > g.n-1){
+        return ERROR;
+    }
+    s = (int*)malloc(g.n*sizeof(int));
+    for(i = 0;i < g.n;i ++){      //初始化
+        s[i] = 0;                 //表示顶点i是否在s中
+        d[i] = g.a[v][i];         //v到i的距离
+        if(i != v && d[i] < INFTY){
+            path[i] = v;          //标识指向i的源点v
+        }
+        else path[i] = -1;
+    }
+    s[v] = 1;                     //顶点v为源点,将原点v加入集合S
+    printf("%d ",v);              //输出源点0
+    d[v] = 0;
+    for(i = 1;i <= g.n-1;i ++){   //产生n-1条最短路径,<改为<=
+        k = Choose(d,g.n,s);      //求当前路径最短者k
+        s[k] = 1;                 //将k加入集合S中
+        printf("%d ",k);
+        for(w = 0;w < g.n;w ++){  //更新d和path
+            if(!s[w] && d[k]+g.a[k][w] < d[w]){
+                d[w] = d[k] + g.a[k][w];
+                distance = d[w];  //计算min距离
+                path[w] = k;
+            }
+        }
+    }
+    printf("\nThe shortest distance:%d ",distance);
+    return OK;
+}
+
+
+
+int main(){
+    mGraph g;
+    int nSize,edge,u,v,i;
+    int d[100];
+    int path[100];
+    ElemType w;
+    printf("Please enter the size of the mgraph:");
+    scanf("%d",&nSize);
+    Init(&g,nSize,INFTY);
+    printf("Please enter the number of the edges:");
+    scanf("%d",&edge);
+    printf("Now init the graph.\n");
+    for(i = 0;i < edge;i ++){
+        printf("Please enter the edge:");
+        scanf("%d%d%d",&u,&v,&w);
+        Insert(&g,u,v,w);
+    }
+    Dijkstra(g,0,d,path);
+    return 0;
+}
